ecd184994d12fc1415e42668a96f8e93ecf8409b
Md. Samiul Islam <138280535+user01samiul@users.noreply.github.com>
2025-08-03 11:54:18 +0600

    dd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["aaatrafficcontrol.com.au"],
+    domains: ["aaatrafficcontrol.com.au", "placehold.co"],
   },
 };
 
